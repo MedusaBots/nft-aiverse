3c1671711653193000fc24c802336a9274c4763e
UPENDRA SINGH <uvsingh2336@gmail.com>
2022-05-06 21:10:56 +0530

    Resized engage box and created engagePage class

--- src/components/Engage.jsx
+++ src/components/Engage.jsx
@@ -1,29 +1,27 @@
-import './engage.css';
+import "./engage.css";
 
 const Engage = () => {
-
-    return(
-
-        <div className="engageParent">
-            <div className="engageTitle">Engage yourself with aiverse</div>
-            <div className="engageStepBox">
-                <div className="engageSteps">
-                    <p className="engageStepNum">01/</p>
-                    <p className="engageStepName">AI Generated Art</p>
-                </div>
-                <div className="engageSteps">
-                    <p className="engageStepNum">02/</p>
-                    <p className="engageStepName">Whitelists & Airdrop</p>
-                </div>
-                <div className="engageSteps">
-                    <p className="engageStepNum">03/</p>
-                    <p className="engageStepName">Get Rewards & Early Access</p>
-                </div>
-                
-            </div>
+  return (
+    <div className="engagePage">
+      <div className="engageParent">
+        <div className="engageTitle">Engage yourself with aiverse</div>
+        <div className="engageStepBox">
+          <div className="engageSteps">
+            <p className="engageStepNum">01/</p>
+            <p className="engageStepName">AI Generated Art</p>
+          </div>
+          <div className="engageSteps">
+            <p className="engageStepNum">02/</p>
+            <p className="engageStepName">Whitelists & Airdrop</p>
+          </div>
+          <div className="engageSteps">
+            <p className="engageStepNum">03/</p>
+            <p className="engageStepName">Get Rewards & Early Access</p>
+          </div>
         </div>
-
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Engage;
